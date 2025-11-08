e5f2639471ec7526438b92cc7966b7e24fff878c
Burak felek <felekburak8@gmail.com>
2025-11-08 05:03:34 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const ProfileGallery = () => {
         "/images/ahu3.jpg",
         "/images/ahu4.jpg"
       ],
-      bio: "Seyahat etmeyi ve fotoğrafçılığı seven bir grafik tasarımcı."
+      bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
       id: 2,
